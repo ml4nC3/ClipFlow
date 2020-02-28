1bbbf46ac5b472e1fec34f0847bf6af318c2bbde
ml4nC3 <nicolas.lance@stfelixlasalle.fr>
2020-02-28 10:46:18 +0100

    Added green led blink for 240s on simulation start

--- ClipFlowSimulator.py
+++ ClipFlowSimulator.py
@@ -44,20 +44,24 @@ class MainWindow:
         self._off_time_pressed = None
         self._timer = QTimer()  # Création du timer de cadencement du jeu
         self.serial_com = None  # Création de l'attribut destiné à l'objet série
-        self.serial_settings = {'PORT': 'COM2', 'BAUDRATE': 9600, 'TIMEOUT': 1}  # Paramètres liaison série
+        self._serial_settings = {'PORT': 'COM2', 'BAUDRATE': 9600, 'TIMEOUT': 1}  # Paramètres liaison série
+        self._starter_time = None
 
         # Définition des couleurs du levier
         self._lever_styles = {'Baissé': 'background-color: rgb(85, 170, 127);\ncolor: rgb(240, 240, 240);',
                               'Levé': 'background-color: rgb(170, 0, 0);\ncolor: rgb(240, 240, 240);'}
 
         # Création des caractéristiques de la LED verte
-        self._green_led_state = False
-        self._green_led_count = 0
-        self._green_led_blink_timer = QTimer()
+        self._green_led_last_blink = None       # Timestamp de la dernière séquence de cligno (évite un timer supp)
+        self._green_led_state = False           # Etat actuel de la LED (False = éteinte / True = allumée)
+        self._green_led_count = 0               # Nombre de changement d'état restants à réaliser
+        self._green_led_blink_timer = QTimer()  # Timer pour gérer les clignotements
         self._green_led_styles = {True: 'background-color: rgb(85, 255, 127);',
-                                  False: 'background-color: rgb(0, 60, 0);'}
+                                  False: 'background-color: rgb(0, 60, 0);'} # Couleurs de fond des objets label
 
         # Création des caractéristiques de la LED rouge
+        self._flag_3_red_blinks = False     # Flag pour éviter la répétition des 3 blinks (voir on_timer_top)
+        self._red_led_last_blink = None
         self._red_led_state = False
         self._red_led_count = 0
         self._red_led_blink_timer = QTimer()
@@ -79,17 +83,19 @@ class MainWindow:
     def on_button_lever_click(self):
         # Démarrage de la liaison série
         try:
-            self.serial_com = serial.Serial(self.serial_settings['PORT'],
-                                            baudrate=self.serial_settings['BAUDRATE'],
-                                            timeout=self.serial_settings['TIMEOUT'])
+            self.serial_com = serial.Serial(self._serial_settings['PORT'],
+                                            baudrate=self._serial_settings['BAUDRATE'],
+                                            timeout=self._serial_settings['TIMEOUT'])
             logging.info("COM port created: " + str(self.serial_com))
         except Exception as error:
             self.ui.lbl_com_status_text.setText("Erreur port série")
-            self.ui.lbl_com_status_text.setStyleSheet('background-color: rgb(255, 0, 0);')
+            self.ui.lbl_com_status_text.setStyleSheet('background-color: rgb(255, 231, 232);')
             logging.error("Failed to start COM port: " + str(error))
 
         # Création/paramétrage des objets
         self.flow_meter = FlowMeter()
+        self._starter_time = timer()
+        self._green_led_last_blink = self._starter_time
 
         # Paramétrage de l'interface
         self.ui.btn_lever.setEnabled(False)
@@ -100,11 +106,30 @@ class MainWindow:
 
     def on_timer_top(self):
         """Méthode appelée à chaque top du Timer"""
+        simulation_duration = int(timer() - self._starter_time)
+
+        # Détection de l'appui long sur le bouton OFF
         if self._off_time_pressed is not None:
-            pass
+            current_duration = int(timer() - self._off_time_pressed)
+            self.ui.lbl_time_count.setText(str(current_duration))
+            if current_duration >= 8 and not self._flag_3_red_blinks:
+                self._flag_3_red_blinks = True
+                self._red_led_count = 6
+                self._red_led_blink_timer.start(300)
+        else:
+            self.ui.lbl_time_count.setText('')
+
+        # Clignotement LED verte pendant 4 minutes
+        if simulation_duration < 240:
+            last_green_blink = timer() - self._green_led_last_blink
+            if last_green_blink >= 5:
+                self._green_led_count = 2  # La LED doit faire 1 cligno donc 2 changements d'état
+                self._green_led_last_blink = timer()
+                self._green_led_blink_timer.start(300)
+
         current_flow = self.flow_meter.flow_measure()
         if current_flow > 0:
-            logging.debug(f"current flowrate measure : " + str(current_flow))
+            logging.debug(f"current flowrate measure : {str(current_flow)}")
 
     def on_btn_off_pressed(self):
         self._off_time_pressed = timer()
@@ -113,26 +138,27 @@ class MainWindow:
         released_time = timer()
         off_pressed_duration = released_time - self._off_time_pressed
         self._off_time_pressed = None
-        logging.info(f"OFF button released at {str(released_time)} with duration {str(off_pressed_duration)}")
+        logging.debug(f"OFF button released at {str(released_time)} with duration {str(off_pressed_duration)}")
         # Comportement en fonction du temps d'appui du bouton
-        if int(off_pressed_duration) > 5:
-            self._red_led_count = 6
-            self._red_led_blink_timer.start(300)
+        if int(off_pressed_duration) >= 8:
             self.lever_trigger()
 
     def on_serial_parameter_change(self):
-        self.serial_settings['PORT'] = self.ui.lineEdit_com_port.text()
-        self.serial_settings['BAUDRATE'] = self.ui.spinBox_baudrate.value()
+        self._serial_settings['PORT'] = self.ui.lineEdit_com_port.text()
+        self._serial_settings['BAUDRATE'] = self.ui.spinBox_baudrate.value()
 
     def green_led_blink(self):
         self._green_led_state = not self._green_led_state
         self.ui.lbl_green_led.setStyleSheet(self._green_led_styles[self._green_led_state])
+        self._green_led_count -= 1
+        if self._green_led_count <= 0:
+            self._green_led_blink_timer.stop()
 
     def red_led_blink(self):
         self._red_led_state = not self._red_led_state
         self.ui.lbl_red_led.setStyleSheet(self._red_led_styles[self._red_led_state])
         self._red_led_count -= 1
-        if self._red_led_count == 0:
+        if self._red_led_count <= 0:
             self._red_led_blink_timer.stop()
 
     def lever_trigger(self):
@@ -141,6 +167,9 @@ class MainWindow:
         # TODO détruire flowmeter
         if self.serial_com is not None and self.serial_com.isOpen():
             self.serial_com.stop()
+
+        # Réinitialisation des paramètres
+        self._flag_3_red_blinks = False
 
         # Mise à jour de l'interface
         self.ui.btn_lever.setEnabled(True)
